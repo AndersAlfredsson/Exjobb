b7621a267eb8175380f4611afb1b1aeeb5751cdb
Anders <anders.alfredsson@gmail.com>
2016-04-14 11:30:30 +0200

    link

--- src/com/company/Modelclasses/ApplicationUser.java
+++ src/com/company/Modelclasses/ApplicationUser.java
@@ -7,6 +7,7 @@ public class ApplicationUser {
     private int ID;
     private String email;
     private String password;
+    //http://www.tutorialspoint.com/design_pattern/data_access_object_pattern.htm
 
     public ApplicationUser(String email, String password){
         this.email = email;
