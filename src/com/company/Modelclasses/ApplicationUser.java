c43623fb401221ed4ab1ddd531f24d1275ee56e7
Anders <anders.alfredsson@gmail.com>
2016-04-14 12:01:00 +0200

    added classes for DAO

--- src/com/company/Modelclasses/ApplicationUser.java
+++ src/com/company/Modelclasses/ApplicationUser.java
@@ -2,6 +2,7 @@ package com.company.Modelclasses;
 
 /**
  * Created by Anders on 2016-04-14.
+ * Class representing an android application user.
  */
 public class ApplicationUser {
     private int ID;
@@ -11,6 +12,31 @@ public class ApplicationUser {
 
     public ApplicationUser(String email, String password){
         this.email = email;
+        this.password = password;
+    }
+
+    //Getters and Setters
+    public int getID() {
+        return ID;
+    }
+
+    public void setID(int ID) {
+        this.ID = ID;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
         this.password = password;
     }
 }
