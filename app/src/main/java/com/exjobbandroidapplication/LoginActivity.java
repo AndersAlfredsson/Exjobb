d22ac9d061ba73aad74d389de0eb8f469782b9cf
Anders <anders.alfredsson@gmail.com>
2016-04-25 14:22:20 +0200

    connection to server now working

--- app/src/main/java/com/exjobbandroidapplication/LoginActivity.java
+++ app/src/main/java/com/exjobbandroidapplication/LoginActivity.java
@@ -1,4 +1,4 @@
-package com.ExjobbAndroidApplication;
+package com.exjobbandroidapplication;
 
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
@@ -29,8 +29,17 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import com.ExjobbAndroidApplication.R;
+
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
 import java.util.ArrayList;
 import java.util.List;
+
+import NetworkMessages.LoginMessage;
+import NetworkMessages.ServerMessage;
 
 import static android.Manifest.permission.READ_CONTACTS;
 
@@ -44,13 +53,6 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
      */
     private static final int REQUEST_READ_CONTACTS = 0;
 
-    /**
-     * A dummy authentication store containing known user names and passwords.
-     * TODO: remove after connecting to a real authentication system.
-     */
-    private static final String[] DUMMY_CREDENTIALS = new String[]{
-            "foo@example.com:hello", "bar@example.com:world"
-    };
     /**
      * Keep track of the login task to ensure we can cancel it if requested.
      */
@@ -306,22 +308,25 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
 
         @Override
         protected Boolean doInBackground(Void... params) {
-            // TODO: attempt authentication against a network service.
-
+            Socket socket = null;
             try {
-                // Simulate network access.
-                Thread.sleep(2000);
-            } catch (InterruptedException e) {
-                return false;
+                socket = new Socket("10.22.5.224", 9058);
+                ObjectOutputStream OUT = new ObjectOutputStream(socket.getOutputStream());
+                ObjectInputStream IN = new ObjectInputStream(socket.getInputStream());
+
+                OUT.writeObject(new LoginMessage("dev@dev.com", "dev"));
+
+                try {
+                    ServerMessage serverMessage =(ServerMessage)IN.readObject();
+                } catch (ClassNotFoundException e) {
+                    e.printStackTrace();
+                }
+
+
+            } catch (IOException e) {
+                e.printStackTrace();
             }
 
-            for (String credential : DUMMY_CREDENTIALS) {
-                String[] pieces = credential.split(":");
-                if (pieces[0].equals(mEmail)) {
-                    // Account exists, return true if the password matches.
-                    return pieces[1].equals(mPassword);
-                }
-            }
 
             // TODO: register the new account here.
             return true;
